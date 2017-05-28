1c503130d2022d376403409fbd65c8bd1f418625
Matthew Emes <memes@matthewemes.com>
2017-05-28 10:36:37 -0700

    Chrome: switch to Chrome from Chromium  - use google-chrome for mail  - add google-chrome to i3  - ignore dialogs when assigning browser windows to workspaces

--- bin/launch_mail.sh
+++ bin/launch_mail.sh
@@ -2,4 +2,5 @@
 #
 # Launch a browser and open webmail
 #EMAIL_SCALING=${EMAIL_SCALING:-1.7}
-chromium --user-data-dir=${HOME}/.config/chromium.mail ${EMAIL_SCALING:+"--force-device-scale-factor=${EMAIL_SCALING}"} > /dev/null 2>/dev/null
+#chromium --user-data-dir=${HOME}/.config/chromium.mail ${EMAIL_SCALING:+"--force-device-scale-factor=${EMAIL_SCALING}"} > /dev/null 2>/dev/null
+google-chrome --user-data-dir=${HOME}/.config/google-chrome.mail ${EMAIL_SCALING:+"--force-device-scale-factor=${EMAIL_SCALING}"} > /dev/null 2>/dev/null
