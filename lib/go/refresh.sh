694963324b352ce432f6ecb4d03c60bbe1cf17d3
Matthew Emes <memes@matthewemes.com>
2017-06-29 10:04:15 -0700

    Go: use hugo from repo, not OS package

--- lib/go/refresh.sh
+++ lib/go/refresh.sh
@@ -27,6 +27,7 @@ golang.org/x/tools/cmd/guru
 github.com/cloudflare/cfssl/cmd/...
 github.com/constabulary/gb/...
 github.com/derekparker/delve/cmd/dlv
+github.com/gohugoio/hugo
 github.com/golang/dep/cmd/dep
 github.com/kisielk/errcheck
 github.com/nsf/gocode
