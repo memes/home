4ab665994ae28df94ce1844b199c63e3b52e40b7
Matthew Emes <memes@matthewemes.com>
2018-11-15 09:56:32 -0800

    zsh: include pip aliases that were missing

--- .profile.d/python/python.zsh
+++ .profile.d/python/python.zsh
@@ -2,6 +2,9 @@
 
 # Python: require a virtualenv by default and integrate with shell
 export PIP_REQUIRE_VIRTUALENV=true
+alias global_pip='PIP_REQUIRE_VIRTUALENV="" pip'
+alias global_pip2='PIP_REQUIRE_VIRTUALENV="" pip2'
+alias global_pip3='PIP_REQUIRE_VIRTUALENV="" pip3'
 
 # Source virtualenvwrapper if found
 _VIRTUALENVWRAPPER=$(command -v virtualenvwrapper.sh)
