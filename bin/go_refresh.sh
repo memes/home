efb1697562498b1ce08f30cb1ddbc1d35eab6bcb
Matthew Emes <memes@matthewemes.com>
2019-02-18 11:25:25 -0800

    Go: remove Powershell version, focus only on shell script  - updated packages  - removed unused packages

--- bin/go_refresh.sh
+++ bin/go_refresh.sh
@@ -1,50 +1,53 @@
 #!/bin/sh
 #
 # Install common go packages to local GOPATH
-case "$(uname)" in
-    Darwin)
-        _GOPATH="${HOME}/Library/go"
-        ;;
+set -e
+command -v local_lib_path >/dev/null 2>/dev/null || . ~/.profile.d/functions/local_lib_path
 
-    Linux)
-        _GOPATH="${HOME}/lib/go"
-        ;;
-
-    *)
-        _GOPATH="does/not/exist"
-        ;;
-esac
-
+_GOPATH=$(local_lib_path go)
 mkdir -p ${_GOPATH}
 
 ECHO="$(which echo)" || "echo"
-# Update packages
-while read p; do
-    ${ECHO} -n "Fetching/updating ${p} "
-    GOPATH=${_GOPATH} go get -u ${p} && ${ECHO} "done"
-done <<EOF
-golang.org/x/oauth2/google
+# Install/update packages
+awk '!/^($|#)/ {print}' <<EOF |
+# Format of list
+#
+# package [GO111MODULE flag]
+#
+# Coding tools
 golang.org/x/tools/cmd/godoc
 golang.org/x/tools/cmd/goimports
 golang.org/x/tools/cmd/gorename
+golang.org/x/tools/cmd/gotype
 golang.org/x/tools/cmd/guru
-github.com/alecthomas/gometalinter
-github.com/cloudflare/cfssl/cmd/...
-github.com/constabulary/gb/...
-github.com/cweill/gotests
-github.com/derekparker/delve/cmd/dlv
-github.com/gohugoio/hugo
-github.com/golang/dep/cmd/dep
-github.com/josharian/impl
-github.com/mdempsky/gocode
+#github.com/mdempsky/gocode
+github.com/stamblerre/gocode
 github.com/rogpeppe/godef
+github.com/cweill/gotests/...
+github.com/davidrjenni/reftools/cmd/fillstruct
+github.com/derekparker/delve/cmd/dlv
+github.com/fatih/gomodifytags
+github.com/godoctor/godoctor
+github.com/golangci/golangci-lint/cmd/golangci-lint
+#github.com/haya14busa/gopkgs/cmd/gopkgs
+github.com/uudashr/gopkgs/cmd/gopkgs
+github.com/josharian/impl
+github.com/zmb3/gogetdoc
+gopkg.in/check.v1
+
+# LSP implementation
 github.com/sourcegraph/go-langserver
-github.com/tools/godep
-google.golang.org/appengine
-google.golang.org/appengine/log
-google.golang.org/api/cloudiot/v1
+
+# Utilities
+github.com/cloudflare/cfssl/cmd/... off
+github.com/gohugoio/hugo
+github.com/golang/dep/cmd/dep off
 EOF
 
-unset _GOPATH
+while read p m; do
+    ${ECHO} "Fetching/updating ${p}"
+    env GOPATH=${_GOPATH} ${m:+"GO111MODULE=${m}"} go get -u ${p} && \
+        ${ECHO} "${p} done"
+done
 
-gometalinter --install
+unset _GOPATH
