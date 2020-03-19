bb76d4bacca50730ba558715ec300d6542c9e8fa
Matthew Emes <memes@matthewemes.com>
2020-03-19 13:19:32 -0700

    Updated shell configuration for Linux  - use completion and binding configs from package  - load the functions correctly under BASH

--- .profile.d/fzf/completion.sh
+++ .profile.d/fzf/completion.sh
@@ -4,3 +4,8 @@
 if [ -n "${BASH_VERSION}" ] && [ -f "${HOME}/.fzf.bash" ]; then
     source "${HOME}/.fzf.bash"
 fi
+# Handle completions and bindings if Debian package is installed
+if [ -n "${BASH_VERSION}" ] && [ -d /usr/share/doc/fzf/examples ]; then
+    source /usr/share/doc/fzf/examples/key-bindings.bash
+    source /usr/share/doc/fzf/examples/completion.bash
+fi
--- .profile.d/fzf/completion.zsh
+++ .profile.d/fzf/completion.zsh
@@ -2,3 +2,6 @@
 #
 # Source ~/.fzf.zsh if present
 [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
+# Handle completions and bindings if Debian package is installed
+[[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ]] && source /usr/share/doc/fzf/examples/key-bindings.zsh
+[[ -f /usr/share/doc/fzf/examples/completion.zsh ]] && source /usr/share/doc/fzf/examples/completion.zsh
