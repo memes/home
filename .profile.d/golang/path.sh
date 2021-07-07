e9dc6a74271dbd73d8f75de9a4840a88c84b80c6
Matthew Emes <memes@matthewemes.com>
2021-07-07 09:14:41 -0700

    Chore: GO updates
     - Prefer to use a tools.go for protoc plugins
     - Set GOBIN in profile
     - GO111MODULES is on by default

--- .profile.d/golang/path.sh
+++ .profile.d/golang/path.sh
@@ -5,6 +5,7 @@
 _LOCAL_GOPATH="$(local_lib_path go)"
 if [ -d "${_LOCAL_GOPATH}" ]; then
     export GOPATH="${_LOCAL_GOPATH}${GOPATH:+":${GOPATH}"}"
+    export GOBIN="${_LOCAL_SDK}/bin"
     export PATH="${_LOCAL_GOPATH}/bin${PATH:+":${PATH}"}"
 fi
 unset _LOCAL_GOPATH
