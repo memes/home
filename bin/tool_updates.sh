3ae4f2f82c3edc7f5ac33ccd742f0e070d78ea78
Matthew Emes <memes@matthewemes.com>
2021-09-01 11:38:03 -0700

    Linux: use go 1.17, if installed, make tool_updates script platform ready

--- bin/tool_updates.sh
+++ bin/tool_updates.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 #
 # Update various things
-brew update
-brew upgrade
-brew cleanup
-brew doctor
+if command -v brew 2>/dev/null >/dev/null; then
+    brew update
+    brew upgrade
+    brew cleanup
+    brew doctor
+fi
+if command -v apt 2>/dev/null >/dev/null; then
+    sudo apt update
+    sudo apt upgrade
+fi
 antibody update
-gcloud components update
+# Don't try to update GCP SDK on Debian
+command -v apt 2>/dev/null >/dev/null || \
+    gcloud components update
 ~/bin/go_refresh.sh
