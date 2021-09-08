b4002b9721fbed553789ef7ef15728ef965ea013
Matthew Emes <memes@matthewemes.com>
2021-09-08 14:12:32 -0700

    Chore: determine Python3.8 pyenv for gsutil

--- .profile.d/google-cloud-sdk/google-cloud-sdk.sh
+++ .profile.d/google-cloud-sdk/google-cloud-sdk.sh
@@ -1,5 +1,10 @@
 #-*- mode: sh -*-
 #
-if command -v brew >/dev/null 2>/dev/null; then
-    export CLOUDSDK_PYTHON=$(brew --prefix python@3.8)/bin/python3.8
+if command -v pyenv >/dev/null 2>/dev/null; then
+    _PYTHON38="$(pyenv prefix `pyenv whence python3.8 | sort -nr | head -n 1`)/bin/python"
+else
+    _PYTHON38="$(which python3.8 2>/dev/null)"
+fi
+if [[ -n "${_PYTHON38}" ]] && [[ -x "${_PYTHON38}" ]]; then
+    export CLOUDSDK_PYTHON="${_PYTHON38}"
 fi
