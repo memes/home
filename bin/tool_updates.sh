aaeae3c632a7689906e3ff3b8b5cb8a54b753622
Matthew Emes <m.emes@f5.com>
2021-09-09 13:47:08 -0700

    Chore: use apt-get command as trigger for apt updates

--- bin/tool_updates.sh
+++ bin/tool_updates.sh
@@ -7,12 +7,12 @@ if command -v brew 2>/dev/null >/dev/null; then
     brew cleanup
     brew doctor
 fi
-if command -v apt 2>/dev/null >/dev/null; then
-    sudo apt update
-    sudo apt upgrade
+if command -v apt-get 2>/dev/null >/dev/null; then
+    sudo apt-get update
+    sudo apt-get upgrade
 fi
 antibody update
 # Don't try to update GCP SDK on Debian
-command -v apt 2>/dev/null >/dev/null || \
+command -v apt-get 2>/dev/null >/dev/null || \
     gcloud components update
 ~/bin/go_refresh.sh
