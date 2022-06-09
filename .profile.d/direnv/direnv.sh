c1fdf1c51307ce8d784aa5649b294ac85654abb5
Matthew Emes <memes@matthewemes.com>
2022-06-09 12:22:42 -0700

    Chore: asdf/direnv integration changes
    - latest direnv/asdf integration on linux changes the install mechanism
    - needs to be validated on macOS

--- .profile.d/direnv/direnv.sh
+++ .profile.d/direnv/direnv.sh
@@ -2,7 +2,9 @@
 #
 # Load direnv support
 
-if command -v asdf >/dev/null 2>/dev/null; then
+if test -f "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/bashrc"; then
+    source "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/bashrc"
+elif command -v asdf >/dev/null 2>/dev/null; then
     eval "$(asdf exec direnv hook bash)"
 elif command -v direnv >/dev/null 2>/dev/null; then
     eval "$(direnv hook bash)"
--- .profile.d/direnv/direnv.zsh
+++ .profile.d/direnv/direnv.zsh
@@ -2,7 +2,9 @@
 #
 # Integrate direnv to the shell
 
-if command -v asdf >/dev/null 2>/dev/null; then
+if test -f "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc"; then
+    source "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc"
+elif command -v asdf >/dev/null 2>/dev/null; then
     eval "$(asdf exec direnv hook zsh)"
 elif command -v direnv >/dev/null 2>/dev/null; then
     eval "$(direnv hook zsh)"
