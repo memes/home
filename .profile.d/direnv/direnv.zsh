2434a0d5dca5ed4f5243a29dfba14d30b734dc7b
Matthew Emes <memes@matthewemes.com>
2022-06-09 12:36:11 -0700

    Chore: update for asdf/direnv tested on macOS

--- .profile.d/direnv/direnv.zsh
+++ .profile.d/direnv/direnv.zsh
@@ -1,6 +1,11 @@
 # -*- mode: sh; eval: (sh-set-shell "zsh") -*-
 #
 # Integrate direnv to the shell
+#  1. install asdf
+#  2. asdf plugin add direnv
+#  3. asdf install direnv latest
+#  4. asdf direnv setup --version latest
+#  5. Remove added line from .zshrc/.bashrc
 
 if test -f "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc"; then
     source "${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc"
