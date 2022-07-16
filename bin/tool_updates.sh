06a89ee1eb0334f545d151932008a2b6dd6648f7
Matthew Emes <memes@matthewemes.com>
2022-07-15 17:55:00 -0700

    Chore: remove antibody update from tool_updates.sh

--- bin/tool_updates.sh
+++ bin/tool_updates.sh
@@ -11,8 +11,14 @@ if command -v apt-get 2>/dev/null >/dev/null; then
     sudo apt-get update
     sudo apt-get upgrade
 fi
-antibody update
 # Don't try to update GCP SDK on Debian
 command -v apt-get 2>/dev/null >/dev/null || \
     gcloud components update
 ~/bin/go_refresh.sh
+
+# Refresh kpt completion
+if command -v kpt 2>/dev/null >/dev/null; then
+    kpt completion zsh > ~/.profile.d/functions/_kpt
+    mkdir -p ~/.profile.d/kpt && \
+        kpt completion bash > ~/.profile.d/kpt/completion.sh
+fi
