fd5b6aa603e81a5df38dd9515a0474e3d517de57
Matthew Emes <memes@matthewemes.com>
2022-09-13 09:35:28 -0700

    Edgeforge: efctl completion

---
+++ .profile.d/edgeforge/completion.sh
@@ -0,0 +1,2 @@
+
+EFCTL_AC_BASH_SETUP_PATH=/Users/memes/Library/Caches/efctl/autocomplete/bash_setup && test -f $EFCTL_AC_BASH_SETUP_PATH && source $EFCTL_AC_BASH_SETUP_PATH; # efctl autocomplete setup
