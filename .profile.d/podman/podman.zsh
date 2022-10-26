b6f6e9661511063006abdc7d93cbd75d4ac87d47
Matthew Emes <memes@matthewemes.com>
2022-10-25 18:08:25 -0700

    Chore: set kpt runtime to podman automatically

--- .profile.d/podman/podman.zsh
+++ .profile.d/podman/podman.zsh
@@ -4,4 +4,5 @@
 
 if command -v podman >/dev/null 2>/dev/null; then
     export DOCKER_HOST=unix:///${HOME}/.local/share/containers/podman/machine/podman-machine-default/podman.sock
+    export KPT_FN_RUNTIME=podman
 fi
