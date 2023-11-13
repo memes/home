c3e761516d8cea28a126e9cea07af81cff331e84
Matthew Emes <memes@matthewemes.com>
2023-11-13 10:05:57 -0800

    fix: Set DOCKER_HOST variable correctly
    Remove extra '/' from DOCKER_HOST variable when setting up shell.

--- .profile.d/podman/podman.sh
+++ .profile.d/podman/podman.sh
@@ -3,7 +3,7 @@
 # Load direnv support
 
 if command -v podman >/dev/null 2>/dev/null; then
-    export DOCKER_HOST=unix:///${HOME}/.local/share/containers/podman/machine/qemu/podman.sock
+    export DOCKER_HOST=unix://${HOME}/.local/share/containers/podman/machine/qemu/podman.sock
     export KPT_FN_RUNTIME=podman
     export KIND_EXPERIMENTAL_PROVIDER=podman
 fi
