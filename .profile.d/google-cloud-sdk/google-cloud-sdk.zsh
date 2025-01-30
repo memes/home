40d0d331ea046e7dafc245f189b8581c30b46782
Matthew Emes <m.emes@f5.com>
2025-01-30 14:29:55 -0800

    fix: Use homebrew python3.11 for GCLOUD SDK
    I usually install Google Cloud SDK via brew which brings in python3.11
    as a dependency. The gsutil bug that put a dependency on python3.8 is
    no longer a thing - or at least it works for me. This change sets up
    the gcloud CLI to use the homebrew python.

--- .profile.d/google-cloud-sdk/google-cloud-sdk.zsh
+++ .profile.d/google-cloud-sdk/google-cloud-sdk.zsh
@@ -1,12 +1,14 @@
 #-*- mode: sh; eval: (sh-set-shell "zsh") -*-
 #
-if command -v asdf >/dev/null 2>/dev/null; then
-    _PYTHON38="$(asdf which python3.8)"
+if command -v brew > /dev/null 2>/dev/null; then
+    _SDK_PYTHON="$(brew --prefix)/bin/python3.11"
+elif command -v asdf >/dev/null 2>/dev/null; then
+    _SDK_PYTHON="$(asdf which python3.8)"
 elif command -v pyenv >/dev/null 2>/dev/null; then
-    _PYTHON38="$(pyenv prefix `pyenv whence python3.8 | sort -nr | head -n 1`)/bin/python"
+    _SDK_PYTHON="$(pyenv prefix `pyenv whence python3.8 | sort -nr | head -n 1`)/bin/python"
 else
-    _PYTHON38="$(which python3.8 2>/dev/null)"
+    _SDK_PYTHON="$(which python3.8 2>/dev/null)"
 fi
-if [ -n "${_PYTHON38}" ] && [ -x "${_PYTHON38}" ]; then
-    export CLOUDSDK_PYTHON="${_PYTHON38}"
+if [ -n "${_SDK_PYTHON}" ] && [ -x "${_SDK_PYTHON}" ]; then
+    export CLOUDSDK_PYTHON="${_SDK_PYTHON}"
 fi
