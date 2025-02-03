e5b3d2a8a3c03c5ed2a3906652ed81a2ebc94604
Matthew Emes <m.emes@f5.com>
2025-02-03 07:24:51 -0800

    fix(shell): Test before including asdf from brew
    Profile was always trying to include asdf if brew was present, without
    checking to see if asdf was installed in brew path.

--- .profile.d/asdf/asdf.sh
+++ .profile.d/asdf/asdf.sh
@@ -2,7 +2,7 @@
 #
 # Load asdf support
 
-if command -v brew >/dev/null 2>/dev/null; then
+if command -v brew >/dev/null 2>/dev/null && test -f "$(brew --prefix asdf)/libexec/asdf.sh"; then
     source "$(brew --prefix asdf)/libexec/asdf.sh"
 elif test -f "${HOME}/.asdf/asdf.sh"; then
     source "${HOME}/.asdf/asdf.sh"
