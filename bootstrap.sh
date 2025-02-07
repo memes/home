48b007808b13fbab80f0d1116d1b2799df010add
Matthew Emes <memes@matthewemes.com>
2025-02-07 09:58:40 -0800

    feat: Try to disable oh-my-zsh on devcontainers

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,26 +11,33 @@ error()
     exit 1
 }
 
+info()
+{
+    echo "$0: INFO: $*" >&2
+}
+
 [ -n "${TARGET_DIR}" ] || error "Target directory does not exist"
 [ -d "${TARGET_DIR}" ] || error "Target is not a directory: ${TARGET_DIR}"
 
+info "Copying/updating files in ${TARGET_DIR}"
 rsync -avh \
-    --exclude ".git" \
-    --exclude ".github" \
-    --exclude ".DS_Store" \
-    --exclude "bootstrap.sh" \
-    --exclude "LICENSE" \
-    --exclude "README.md" \
-    --exclude "cloudshell.md" \
-    --exclude ".gitignore" \
-    --exclude ".pre-commit-config.yaml" \
-    --exclude ".talismanrc" \
-    --exclude ".yamllint.yaml" \
-    "${SOURCE_DIR}/" "${TARGET_DIR}/"
+        --exclude ".git" \
+        --exclude ".github" \
+        --exclude ".DS_Store" \
+        --exclude "bootstrap.sh" \
+        --exclude "LICENSE" \
+        --exclude "README.md" \
+        --exclude "cloudshell.md" \
+        --exclude ".gitignore" \
+        --exclude ".pre-commit-config.yaml" \
+        --exclude ".talismanrc" \
+        --exclude ".yamllint.yaml" \
+        "${SOURCE_DIR}/" "${TARGET_DIR}/" || \
+    error "Failed to copy/update files in ${TARGET_DIR}: exit code: $?"
 
 if ! grep -q .zshrc_memes "${TARGET_DIR}/.zshrc" 2>/dev/null; then
-        echo "Updating ${TARGET_DIR}/.zshrc"
-    cat <<'ZSHRC' >>"${TARGET_DIR}/.zshrc"
+        info "Updating ${TARGET_DIR}/.zshrc"
+    cat <<'ZSHRC' >>"${TARGET_DIR}/.zshrc" || error "Failed to update ${TARGET_DIR}/.zshrc: exit code: $?"
 
 # Load memes customisations
 [[ -f ~/.zshrc_memes ]] && source ~/.zshrc_memes
@@ -38,46 +45,57 @@ ZSHRC
 fi
 
 if ! grep -q .profile_memes "${TARGET_DIR}/.profile" 2>/dev/null; then
-    echo "Updating ${TARGET_DIR}/.profile"
-    cat <<'PROFILE' >>"${TARGET_DIR}/.profile"
+    info "Updating ${TARGET_DIR}/.profile"
+    cat <<'PROFILE' >>"${TARGET_DIR}/.profile" || error "Failed to update ${TARGET_DIR}/.profile: exit code: $?"
 
 # Load memes customisations
 if [ -f "${HOME}/.profile_memes" ]; then
     . "${HOME}/.profile_memes"
 fi
 PROFILE
-
     if [ "$(uname)" = "Darwin" ] && ! grep -q .bashrc "${TARGET_DIR}/.profile" 2>/dev/null; then
-        echo "Sourcing .bashrc in OSX .profile"
-        cat <<'PROFILEOSX' >>"${TARGET_DIR}/.profile"
+        info "Sourcing .bashrc in OSX .profile"
+        cat <<'PROFILEOSX' >>"${TARGET_DIR}/.profile" || error "Failed to update ${TARGET_DIR}/.profile: exit code: $?"
 # Source .bashrc when shell is bash - needed on OSX
 [ -n "${BASH_VERSION}" ] && [ -f "${HOME}/.bashrc" ] && . "${HOME}/.bashrc"
 PROFILEOSX
-
     fi
 fi
 
 if ! grep -q .bashrc_memes "${TARGET_DIR}/.bashrc" 2>/dev/null; then
-        echo "Updating ${TARGET_DIR}/.bashrc"
-    cat <<'BASHRC' >>"${TARGET_DIR}/.bashrc"
+        info "Updating ${TARGET_DIR}/.bashrc"
+    cat <<'BASHRC' >>"${TARGET_DIR}/.bashrc" || error "Failed to update ${TARGET_DIR}/.bashrc: exit code: $?"
 
 # Load memes customisations
 [ -f "${HOME}/.bashrc_memes" ] && . "${HOME}/.bashrc_memes"
 BASHRC
 fi
 
-if printenv | grep -q VSCODE; then
-    echo "Skipping git credential helper in devcontainer"
+# Try to customise for specific environments
+if [ -f /usr/local/etc/vscode-dev-containers/meta.env ] || [ -f /usr/local/etc/dev-containers/meta.env ]; then
+    info "Disabling oh-my-zsh in devcontainer"
+    # shellcheck disable=SC2016
+    sed -i.dotfiles \
+            -e 's/^export ZSH=/# export ZSH=/g' \
+            -e '/^ZSH_THEME/# ZSH_THEME/g' \
+            -e "/^zstyle ':omz:/# zstyle ':omz:/g" \
+            -e '/^ZSH_CUSTOM=/# ZSH_CUSTOM=/g' \
+            -e '/^plugins=/# plugins=/g' \
+            -e '/^source $ZSH\//# source $ZSH\//g' \
+        "${TARGET_DIR}/.zshrc"
 else
+    info "Installing default git credential.helper"
     case "$(uname)" in
         Darwin)
-            git config --file "${TARGET_DIR}/.gitconfig.private" credential.helper osxkeychain
+            git config --file "${TARGET_DIR}/.gitconfig.private" credential.helper osxkeychain || \
+                error "Failed to update ${TARGET_DIR}/.gitconfig.private: exit code: $?"
             ;;
         Linux)
-            git config --file "${TARGET_DIR}/.gitconfig.private" credential.helper store
+            git config --file "${TARGET_DIR}/.gitconfig.private" credential.helper store || \
+                error "Failed to update ${TARGET_DIR}/.gitconfig.private: exit code: $?"
             ;;
         *)
-            echo "Unrecognised OS; not setting a default git credential helper"
+            info "Unrecognised OS; not setting a default git credential helper"
             ;;
     esac
 fi
