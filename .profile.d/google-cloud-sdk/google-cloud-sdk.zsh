bac4d7a36b4eca9581c615324b58217e210367aa
Matthew Emes <memes@matthewemes.com>
2025-12-15 15:08:28 -0800

    fix(gcloud): Select python for gcloud CLI
    * If the default python version is 3.10+ end test early; these versions
      are ok
    * Try to find python v3.14-3.10 using one of the other installation
      methods
    
    Closes #41

--- .profile.d/google-cloud-sdk/google-cloud-sdk.zsh
+++ .profile.d/google-cloud-sdk/google-cloud-sdk.zsh
@@ -1,14 +1,22 @@
 #-*- mode: sh; eval: (sh-set-shell "zsh") -*-
 #
-if command -v brew > /dev/null 2>/dev/null; then
-    _SDK_PYTHON="$(brew --prefix)/bin/python3.12"
-elif command -v asdf >/dev/null 2>/dev/null; then
-    _SDK_PYTHON="$(asdf which python3.12)"
-elif command -v pyenv >/dev/null 2>/dev/null; then
-    _SDK_PYTHON="$(pyenv prefix `pyenv whence python3.8 | sort -nr | head -n 1`)/bin/python"
-else
-    _SDK_PYTHON="$(which python3.12 2>/dev/null)"
-fi
-if [ -n "${_SDK_PYTHON}" ] && [ -x "${_SDK_PYTHON}" ]; then
-    export CLOUDSDK_PYTHON="${_SDK_PYTHON}"
-fi
+
+# If default python is 3.10+ there's nothing left to do
+(python3 --version 2>/dev/null || python --version 2>/dev/null) | \
+    grep -Eq 'Python[ \t]+3\.1[0-9]' 2>/dev/null && return 0
+
+for v in 3.14 3.13 3.12 3.11 3.10; do
+    if command -v brew > /dev/null 2>/dev/null; then
+        _SDK_PYTHON="$(brew --prefix)/bin/python{v}"
+    elif command -v asdf >/dev/null 2>/dev/null; then
+        _SDK_PYTHON="$(asdf which python${v})"
+    elif command -v pyenv >/dev/null 2>/dev/null; then
+        _SDK_PYTHON="$(pyenv prefix "$(pyenv whence python${v} | sort -nr | head -n 1)")/bin/python"
+    else
+        _SDK_PYTHON="$(which python${v} 2>/dev/null)"
+    fi
+    if [ -n "${_SDK_PYTHON}" ] && [ -x "${_SDK_PYTHON}" ]; then
+        export CLOUDSDK_PYTHON="${_SDK_PYTHON}"
+        break
+    fi
+done
