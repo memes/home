fb983ae85d3f56ca0604bfa20a190992487e4345
Matthew Emes <memes@matthewemes.com>
2026-06-19 09:37:05 -0700

    fix: Fzf shell integration
    Add fzf completions for zsh/bash on macOS as these had been broken for a
    while.

--- .profile.d/fzf/completion.sh
+++ .profile.d/fzf/completion.sh
@@ -5,8 +5,15 @@
 if [ -n "${BASH_VERSION}" ] && [ -f "${HOME}/.fzf.bash" ]; then
     source "${HOME}/.fzf.bash"
 fi
-# Handle completions and bindings if Debian package is installed
-if [ -n "${BASH_VERSION}" ] && [ -d /usr/share/doc/fzf/examples ]; then
-    source /usr/share/doc/fzf/examples/key-bindings.bash
-    source /usr/share/doc/fzf/examples/completion.bash
+
+command -v brew >/dev/null 2>/dev/null &&
+    _FZF_DIR="$(brew --prefix fzf)/shell"
+# Fall back to Debian completion dir
+_FZF_DIR="${_FZF_DIR:-/usr/share/doc/fzf/examples}"
+
+# Handle completions and bindings if installed
+if [ -n "${BASH_VERSION}" ]; then
+    [ -f "${_FZF_DIR}/key-bindings.bash" ] && source "${_FZF_DIR}/key-bindings.bash"
+    [ -f "${_FZF_DIR}/completion.bash" ] && source "${_FZF_DIR}/completion.bash"
 fi
+unset _FZF_DIR
--- .profile.d/fzf/completion.zsh
+++ .profile.d/fzf/completion.zsh
@@ -1,7 +1,14 @@
-#-*- mode: sh -*-
+#-*- mode: sh; eval: (sh-set-shell "zsh") -*-
 #
 # Source ~/.fzf.zsh if present
 [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
-# Handle completions and bindings if Debian package is installed
-[[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ]] && source /usr/share/doc/fzf/examples/key-bindings.zsh
-[[ -f /usr/share/doc/fzf/examples/completion.zsh ]] && source /usr/share/doc/fzf/examples/completion.zsh
+
+command -v brew >/dev/null 2>/dev/null &&
+    _FZF_DIR="$(brew --prefix fzf)/shell"
+# Fall back to Debian completion dir
+_FZF_DIR="${_FZF_DIR:-/usr/share/doc/fzf/examples}"
+
+# Handle completions and bindings if installed
+[ -f "${_FZF_DIR}/key-bindings.zsh" ] && source "${_FZF_DIR}/key-bindings.zsh"
+[ -f "${_FZF_DIR}/completion.zsh" ] && source "${_FZF_DIR}/completion.zsh"
+unset _FZF_DIR
