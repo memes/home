1db45ceccf33682f70dedb2f6a187c8e8481f3a8
Matthew Emes <memes@matthewemes.com>
2026-08-05 08:27:35 -0700

    fix: Remove iterm2 and set path to prefer brew
    Attempting to minimize third-party stuff on base OS, so removing iterm2
    in preference for built in terminal utility on macs. However, utilities
    added by brew should be used in preference to native, so add homebrew
    bin folder to path if it exists.

--- .profile.d/homebrew/path.zsh
+++ .profile.d/homebrew/path.zsh
@@ -1,10 +1,14 @@
 # -*- mode: sh; eval: (sh-set-shell "zsh") -*-
 #
-# If homebrew and zsh-completions are installed, add to fpath
+# If homebrew and zsh-completions are installed, add to fpath and prioritize binaries.
 
 if command -v brew >/dev/null 2>/dev/null; then
-    _LOCAL_ZSH_COMPLETIONS="$(brew --prefix)/share/zsh-completions"
+    _LOCAL_BREW_PREFIX="$(brew --prefix)"
+    [[ -d "${_LOCAL_BREW_PREFIX}/bin" ]] && [[ -x "${_LOCAL_BREW_PREFIX}/bin" ]] && \
+        path=("${_LOCAL_BREW_PREFIX}/bin" $path)
+    _LOCAL_ZSH_COMPLETIONS="${_LOCAL_BREW_PREFIX}/share/zsh-completions"
     [[ -d "${_LOCAL_ZSH_COMPLETIONS}" ]] && \
         fpath=(${_LOCAL_ZSH_COMPLETIONS} $fpath)
     unset _LOCAL_ZSH_COMPLETIONS
+    unset _LOCAL_BRE_PREFIX
 fi
